3ca18ea3fa72e8f267ec84c4d4121efd1c66a389
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-24 01:38:44 +0300

    add color styles

---
+++ src/components/styles/ColorStyles.js
@@ -0,0 +1,18 @@
+export const themes = {
+  light: {
+    text1: '#000000',
+    text2: 'rgba(0, 0, 0, 0.7)',
+    primary: '#3913B8',
+    secondary: '#2FB5FC',
+    background1: '#F2F6FF',
+    background2: '#E3E8FF',
+  },
+  dark: {
+    text1: '#FFFFFF',
+    text2: 'rgba(255, 255, 255, 0.7);',
+    primary: '#8352FD',
+    secondary: '#00CFFD',
+    background1: '#1F1F47',
+    background2: '#1A2151',
+  },
+};
