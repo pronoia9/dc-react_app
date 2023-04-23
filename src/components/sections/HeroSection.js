f0d9a24ca170dad811214437eee0ff8bd271cc23
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-24 00:24:37 +0300

    add initial wrappers

--- src/components/sections/HeroSection.js
+++ src/components/sections/HeroSection.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import styled from 'styled-components';
 
 const HeroSection = () => {
   return (
-    <>
-      <img src='/images/logos/logo.svg' alt='logo' />
-      <h1>Design and code React apps</h1>
-      <p>
-        Don’t skip design. Learn design and code, by building real apps with React and Swift. Complete courses
-        about the best tools.
-      </p>
-      <Link to='/page-2/'>Go to page 2</Link> <br />
-    </>
+    <Wrapper>
+      <ContentWrapper>
+        <img src='/images/logos/logo.svg' alt='logo' />
+        <h1>Design and code React apps</h1>
+        <p>
+          Don’t skip design. Learn design and code, by building real apps with React and Swift. Complete
+          courses about the best tools.
+        </p>
+        <Link to='/page-2/'>Go to page 2</Link> <br />
+      </ContentWrapper>
+    </Wrapper>
   );
 };
 
 export default HeroSection;
+
+// STYLED COMPONENTS
+const Wrapper = styled.div`
+  background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+`;
+
+const ContentWrapper = styled.div`
+  max-width: 1234px;
+  margin: 0 auto;
+  padding: 200px 30px;
+`;
