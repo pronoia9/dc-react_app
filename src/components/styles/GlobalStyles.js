5d21ffceda097f5a5bea32c05dc2362d20a24a18
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-24 01:51:10 +0300

    update background color

--- src/components/styles/GlobalStyles.js
+++ src/components/styles/GlobalStyles.js
@@ -3,10 +3,10 @@ import { themes } from './ColorStyles';
 
 export const GlobalStyle = createGlobalStyle`
   body {
-    background: ${themes.light.background1};
+    background: ${themes.light.backgroundColor};
 
     @media (prefers-color-scheme: dark) {
-      background: ${themes.dark.background1};
+      background: ${themes.dark.backgroundColor};
     }
   }
 `;
