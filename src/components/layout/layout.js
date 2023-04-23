82d43628354ef0446c84680c47954513c34f70fe
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-24 01:48:35 +0300

    add global styles

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,12 +1,14 @@
-import React from "react"
-import "./layout.css"
+import React from 'react';
+import './layout.css';
+import { GlobalStyle } from '../styles/GlobalStyles';
 
 function Layout({ children }) {
   return (
     <>
+      <GlobalStyle />
       <main>{children}</main>
     </>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
