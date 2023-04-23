768fa5eec63afada2c7b5e4020fe9e8b24c8b532
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-23 23:28:13 +0300

    remove init text

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,13 +9,6 @@ function IndexPage() {
     <Layout>
       <Seo title='Home' />
       <HeroSection />
-      <img src='/images/logos/logo.svg' alt='logo' />
-      <h1>Design and code React apps</h1>
-      <p>
-        Don’t skip design. Learn design and code, by building real apps with React and Swift. Complete courses
-        about the best tools.
-      </p>
-      <Link to='/page-2/'>Go to page 2</Link> <br />
     </Layout>
   );
 }
