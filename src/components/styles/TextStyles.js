d5d6b2f558ab502ff9df2551cc7831bca89eedd8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-24 01:32:35 +0300

    update text styles

--- src/components/styles/TextStyles.js
+++ src/components/styles/TextStyles.js
@@ -18,36 +18,43 @@ export const H3 = styled.h3`
 export const BodyIntro = styled.p`
   font-weight: 500;
   font-size: 24px;
+  line-height: 140%;
 `;
 
 export const BodyMain = styled.p`
   font-weight: 400;
   font-size: 20px;
+  line-height: 140%;
 `;
 
 export const MediumText = styled.p`
   font-weight: 400;
   font-size: 17px;
+  line-height: 130%;
 `;
 
 export const Caption = styled.p`
   font-weight: 400;
   font-size: 17px;
+  line-height: 130%;
 `;
 
 export const Caption2 = styled.p`
   font-weight: 600;
   font-size: 15px;
+  line-height: 130%;
   text-transform: uppercase;
 `;
 
 export const SmallText = styled.p`
   font-weight: 400;
   font-size: 13px;
+  line-height: 130%;
 `;
 
 export const SmallText2 = styled.p`
   font-weight: 600;
   font-size: 13px;
+  line-height: 130%;
   text-transform: uppercase;
 `;
