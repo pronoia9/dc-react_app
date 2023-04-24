27cb58d0dec0cda3a0085a689dfb03e6e4d94920
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-24 13:40:36 +0300

    destructure navlinks

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import { Link } from 'gatsby';
+
 import { headerSection } from '../../utils/data';
 
 export default function Header() {
   return (
     <div>
-      {headerSection.navLinks.map((link, index) => (
-        <p key={index}>{link.title}</p>
+      {headerSection.navLinks.map(({ title, icon, url }, index) => (
+        <Link>
+          <img src={icon} />
+          <p key={index}>{title}</p>
+        </Link>
       ))}
     </div>
   );
