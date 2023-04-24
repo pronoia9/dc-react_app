457c92813129ac0f5b8641e51f2081a977187dde
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-24 23:59:37 +0300

    add init header

--- src/components/sections/HeaderSection.jsx
+++ src/components/sections/HeaderSection.jsx
@@ -6,5 +6,53 @@ import { MenuTooltip } from '../tooltips';
 import { headerData } from '../../utils/data';
 
 export default function HeaderSection() {
-  return <div>HeaderSection</div>;
+  const [isOpen, setIsOpen] = useState({ more: false, search: false, account: false });
+
+  useEffect(() => {}, []);
+
+  const handleClick = () => {};
+
+  const handleClickOutside = () => {};
+
+  return <div></div>;
 }
+
+const Wrapper = styled.div`
+  position: absolute;
+  top: 60px;
+  display: grid;
+  grid-template-columns: 44px auto;
+  width: 100%;
+  justify-content: space-between;
+  padding: 0 30px;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    top: 30px;
+  }
+  @media (max-width: 450px) {
+    top: 20px;
+    padding: 0 20px;
+  }
+`;
+
+const MenuWrapper = styled.div`
+  display: grid;
+  gap: 30px;
+  grid-template-columns: repeat(${(props) => props.count}, auto);
+
+  @media (max-width: 768px) {
+    > a {
+      display: none;
+    }
+    grid-template-columns: auto;
+  }
+`;
+
+const HamburgerWrapper = styled.div`
+  display: none;
+
+  @media (max-width: 768px) {
+    display: block;
+  }
+`;
