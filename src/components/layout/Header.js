a7f6e5fb45d90d2ad0db26ee38ed801e12112e41
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-24 13:57:38 +0300

    add styles for wrappers + hover

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -8,14 +8,49 @@ export default function Header() {
   return (
     <Wrapper>
       <img src={headerSection.logo} />
-      {headerSection.navLinks.map(({ title, icon, link }, index) => (
-        <Link to={link} key={index}>
-          <img src={icon} alt={title} />
-          <p>{title}</p>
-        </Link>
-      ))}
+      <MenuWrapper>
+        {headerSection.navLinks.map(({ title, icon, link }, index) => (
+          <Link to={link} key={index}>
+            <MenuItem>
+              <img src={icon} alt={title} />
+              {title}
+            </MenuItem>
+          </Link>
+        ))}
+      </MenuWrapper>
     </Wrapper>
   );
 }
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  position: absolute;
+  top: 60px;
+  display: grid;
+  grid-template-columns: 44px auto;
+  width: 100%;
+  justify-content: space-between;
+  padding: 0 30px;
+  align-items: center;
+`;
+
+const MenuWrapper = styled.div`
+  display: grid;
+  grid-template-columns: repeat(7, auto);
+  gap: 30px;
+`;
+
+const MenuItem = styled.div`
+  color: rgba(255, 255, 255, 0.7);
+  display: grid;
+  grid-template-columns: 24px auto;
+  gap: 10px;
+  align-items: center;
+  padding: 10px;
+  border-radius: 10px;
+  transition: 0.5s ease-out;
+
+  :hover {
+    background: rgba(255, 255, 255, 0.1);
+    box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1), inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.2);
+  }
+`;
