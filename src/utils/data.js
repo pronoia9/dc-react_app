f7c0e67d1a376fcd92f07ba2a9a8b03128fd7354
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-24 03:17:35 +0300

    add ring to button

--- src/utils/data.js
+++ src/utils/data.js
@@ -3,6 +3,7 @@ export const heroSection = {
   description: `Don’t skip design. Learn design and code, by building real apps with React and Swift. Complete courses about the best tools.`,
   button: {
     icon: '/images/icons/credit.svg',
+    ring: '/images/icons/icon-ring.svg',
     title: 'Get Pro Access',
     subtitle: '$19 per month',
   },
