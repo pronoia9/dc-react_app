42950a5ac19f4cf254d1a6ec4288ce8d19215e35
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-24 23:36:50 +0300

    add imports

--- src/components/sections/HeaderSection.jsx
+++ src/components/sections/HeaderSection.jsx
@@ -1,3 +1,10 @@
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+import { MenuButton } from '../buttons';
+import { MenuTooltip } from '../tooltips';
+import { headerData } from '../../utils/data';
+
 export default function HeaderSection() {
   return <div>HeaderSection</div>;
 }
