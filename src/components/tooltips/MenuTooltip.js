95d5fcf2aa0664e61c1017aacb58000e09abd3ac
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-24 14:36:32 +0300

    add init data

--- src/components/tooltips/MenuTooltip.js
+++ src/components/tooltips/MenuTooltip.js
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { MenuButton } from '../buttons';
 
-export default function MenuTooltip() {
-  return <div>MenuTooltip</div>;
+export default function MenuTooltip({ data }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Wrapper isOpen={isOpen} onClick={() => setIsOpen(!isOpen)}>
+      {data.map((item, index) => (
+        <MenuButton key={`${index}-${item.title}`} {...item} index={index} />
+      ))}
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  background: rgba(15, 14, 71, 0.3);
+  box-shadow: 0px 50px 100px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 0.5px rgba(255, 255, 255, 0.2);
+  backdrop-filter: blur(20px);
+  /* Note: backdrop-filter has minimal browser support */
+  border-radius: 20px;
+  padding: 20px;
+  position: absolute;
+  top: 60px;
+  right: 30px;
+  opacity: ${(props) => (props.isOpen ? '1' : '0.5')};
+`;
