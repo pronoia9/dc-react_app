93fc0b95e7a7252e8eb9f8ce6088ac0a3f2196c2
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-25 00:18:53 +0300

    finish waves bg

--- src/components/backgrounds/WaveBackground.jsx
+++ src/components/backgrounds/WaveBackground.jsx
@@ -1,3 +1,40 @@
+import styled from 'styled-components';
+import { heroWave1, heroWave2, heroWave3, heroWave3Dark } from '../../../static/images/waves';
+
 export default function WaveBackground() {
-  return <div>WaveBackground</div>;
+  return (
+    <Wrapper>
+      <Background />
+      <Wave src={heroWave1} style={{ top: '0px' }} />
+      <Wave src={heroWave2} style={{ top: '350px' }} />
+      <Wave src={heroWave3} style={{ top: '550px' }} />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const Background = styled.div`
+  background: linear-gradient(180deg, #4316db 0%, #9076e7 100%);
+  position: absolute;
+  width: 100%;
+  height: 800px;
+  z-index: -1;
+`;
+
+const Wave = styled.img`
+  position: absolute;
+  z-index: -1;
+
+  @media (min-width: 1440px) {
+    width: 100%;
+  }
+
+  :last-child {
+    @media (prefers-color-scheme: dark) {
+      content: url('/static/images/waves/hero-wave3-dark.svg');
+    }
+  }
+`;
