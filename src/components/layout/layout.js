3e1f6d142fb3e98602999312e05064ed69bb8011
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-24 13:35:42 +0300

    add header

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import './layout.css';
+
+import Header from './Header';
 import { GlobalStyle } from '../styles/GlobalStyles';
+import './layout.css';
 
 function Layout({ children }) {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <main>{children}</main>
     </>
   );
