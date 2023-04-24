8459b75759ec5d5a417ff8d9846a091309d8ac64
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-25 00:04:15 +0300

    add imports

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -1,3 +1,11 @@
+import styled, { keyframes } from 'styled-components';
+
+import { MockupAnimations } from '../animations';
+import { WaveBackground } from '../backgrounds';
+import { PurchaseButton } from '../buttons';
+import { themes } from '../styles/ColorStyles';
+import { H1, MediumText } from '../styles/TextStyles';
+
 export default function HeroSection() {
   return <div>HeroSection</div>;
 }
