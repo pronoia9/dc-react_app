30dd57d021a82f9738ac4214abdc86ac5bcdcadc
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-24 03:03:12 +0300

    add image url to button icon

--- src/utils/data.js
+++ src/utils/data.js
@@ -2,7 +2,7 @@ export const heroSection = {
   title: `Design and code React apps`,
   description: `Don’t skip design. Learn design and code, by building real apps with React and Swift. Complete courses about the best tools.`,
   button: {
-    icon: '',
+    icon: '/images/icons/credit.svg',
     title: 'Get Pro Access',
     subtitle: '$19 per month',
   },
