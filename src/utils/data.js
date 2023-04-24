31a94b5455e3fc57952e1b09457c4b1083c8cf29
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-24 13:57:25 +0300

    add more navlinks data

--- src/utils/data.js
+++ src/utils/data.js
@@ -23,6 +23,10 @@ export const headerSection = {
   navLinks: [
     { title: 'Courses', icon: '/images/icons/courses.svg', link: '/courses' },
     { title: 'Tutorials', icon: '/images/icons/tutorials.svg', link: '/tutorials' },
+    { title: 'Livestreams', icon: '/images/icons/livestreams.svg', link: '/livestreams' },
     { title: 'Pricing', icon: '/images/icons/pricing.svg', link: '/pricing' },
+    { title: '', icon: '/images/icons/more.svg', link: '/' },
+    { title: '', icon: '/images/icons/search.svg', link: '/' },
+    { title: '', icon: '/images/icons/account.svg', link: '/' },
   ],
 };
