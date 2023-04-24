6f51a3a7cbcf137d1dec7fb73d47ea2ffe438ab9
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-04-24 19:52:45 +0300

    add mobile font sizes

--- src/components/styles/TextStyles.js
+++ src/components/styles/TextStyles.js
@@ -3,40 +3,68 @@ import styled from 'styled-components';
 export const H1 = styled.h1`
   font-weight: 700;
   font-size: 60px;
+
+  @media (max-width: 450px) {
+    font-size: 34px;
+  }
 `;
 
 export const H2 = styled.h2`
   font-weight: 700;
   font-size: 40px;
+
+  @media (max-width: 450px) {
+    font-size: 28px;
+  }
 `;
 
 export const H3 = styled.h3`
   font-weight: 700;
   font-size: 30px;
+
+  @media (max-width: 450px) {
+    font-size: 24px;
+  }
 `;
 
 export const BodyIntro = styled.p`
   font-weight: 500;
   font-size: 24px;
   line-height: 140%;
+
+  @media (max-width: 450px) {
+    font-size: 20px;
+  }
 `;
 
 export const BodyMain = styled.p`
   font-weight: 400;
   font-size: 20px;
   line-height: 140%;
+
+  @media (max-width: 450px) {
+    font-size: 17px;
+  }
 `;
 
 export const MediumText = styled.p`
   font-weight: 400;
   font-size: 17px;
   line-height: 130%;
+
+  @media (max-width: 450px) {
+    font-size: 17px;
+  }
 `;
 
 export const Caption = styled.p`
   font-weight: 400;
   font-size: 17px;
   line-height: 130%;
+
+  @media (max-width: 450px) {
+    font-size: 15px;
+  }
 `;
 
 export const Caption2 = styled.p`
@@ -44,12 +72,20 @@ export const Caption2 = styled.p`
   font-size: 15px;
   line-height: 130%;
   text-transform: uppercase;
+
+  @media (max-width: 450px) {
+    font-size: 15px;
+  }
 `;
 
 export const SmallText = styled.p`
   font-weight: 400;
   font-size: 13px;
   line-height: 130%;
+
+  @media (max-width: 450px) {
+    font-size: 13px;
+  }
 `;
 
 export const SmallText2 = styled.p`
@@ -57,4 +93,8 @@ export const SmallText2 = styled.p`
   font-size: 13px;
   line-height: 130%;
   text-transform: uppercase;
+
+  @media (max-width: 450px) {
+    font-size: 13px;
+  }
 `;
